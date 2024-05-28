a29e4e61a82455d95e68b096142c855003c82e06
iain <iain@falsevictories.com>
2024-05-28 21:47:39 +0100

    Initial project commit

---
+++ Editor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Editor
+//
+//  Created by iain on 28/05/2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
